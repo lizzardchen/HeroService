241d5b2def0b3bcc04d7401f4d1c37193a838b26
lizzardchen <744013506@qq.com>
2012-12-29 21:12:54 +0800

    test1
    test1

---
+++ logincontroller.php
@@ -0,0 +1,16 @@
+<?php
+
+class logincontroller {
+	
+	public function Loginheros($name,$tick,$time)
+	{
+		return "login ok";
+	}
+	
+	function __construct() {
+	}
+	function __destruct() {
+	}
+}
+
+?>
